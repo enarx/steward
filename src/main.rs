7550e24c8db2a645db0bfbfbf5563d39d43f2354
Nathaniel McCallum <nathaniel@profian.com>
2022-01-25 17:02:42 -0500

    Initial commit
    This commit just contains a basic HTTP end point and a test for that endpoint.
    We will start adding functionality from here.
    
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::net::SocketAddr;
+
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await
+        .unwrap();
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(root))
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode};
+    use tower::ServiceExt; // for `app.oneshot()`
+
+    #[tokio::test]
+    async fn hello_world() {
+        let response = app()
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+}
