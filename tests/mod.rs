cbf4de41819a53ec5c854fcf45f3a5cc0e373ac9
Richard Zak <richard.j.zak@gmail.com>
2022-02-22 14:30:47 -0500

    Replaced proprietary format with X509, worked with OpenSSL

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,28 +1,26 @@
+use hex_literal::hex;
+use sha2::{Digest, Sha256};
 use std::fs;
 use std::io;
-use hex_literal::hex;
-use sha2::{Sha256, Digest};
 
 #[test]
 fn validate_milan_cert() {
-    let mut file = fs::File::open("certs/ask_ark_milan.cert").unwrap();
+    let mut file = fs::File::open("certs/Milan.pem").unwrap();
     let mut hasher = Sha256::new();
     io::copy(&mut file, &mut hasher).expect("Failed to read cert file");
-    assert_eq!(hasher.finalize()[..], hex!("f3315952a077b37c3c7c58aea544a16fc62fe5a06185b33c86a518658bb2086b"))
+    assert_eq!(
+        hasher.finalize()[..],
+        hex!("22e62f8d2c21a156470145fc75f7b5a377cb053ced3e97f0bd3f8d8ca5941ce6")
+    )
 }
 
 #[test]
 fn validate_rome_cert() {
-    let mut file = fs::File::open("certs/ask_ark_rome.cert").unwrap();
+    let mut file = fs::File::open("certs/Genoa.pem").unwrap();
     let mut hasher = Sha256::new();
     io::copy(&mut file, &mut hasher).expect("Failed to read cert file");
-    assert_eq!(hasher.finalize()[..], hex!("9d7e6b96377ab614e2182e0aae0dcde597019fca23716423f4b902f5dc15c0a6"))
-}
-
-#[test]
-fn validate_naples_cert() {
-    let mut file = fs::File::open("certs/ask_ark_naples.cert").unwrap();
-    let mut hasher = Sha256::new();
-    io::copy(&mut file, &mut hasher).expect("Failed to read cert file");
-    assert_eq!(hasher.finalize()[..], hex!("4629ccff0b7dbe64de42925f7d6c97afc60143b4ad027cb6c51e50bfb35ccbf9"))
+    assert_eq!(
+        hasher.finalize()[..],
+        hex!("125e5d458b0f93d6b2006a2175df52cf988fff4d0e7e220cd0186d402bf2c33f")
+    )
 }
