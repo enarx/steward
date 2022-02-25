b9e5eb669b20854a8d0292b3067ad4cd3b507e88
Nathaniel McCallum <nathaniel@profian.com>
2022-02-25 18:30:51 -0500

    feat: add RSA verification support
    Signed-off-by: Nathaniel McCallum <nathaniel@profian.com>

--- src/crypto/spki.rs
+++ src/crypto/spki.rs
@@ -1,8 +1,26 @@
+use super::oids::*;
+
+use der::{Decodable, Sequence};
+use ring::signature::VerificationAlgorithm as VerAlg;
+use ring::signature::*;
 use spki::{AlgorithmIdentifier, SubjectPublicKeyInfo};
 
 use anyhow::{anyhow, Result};
 
-use super::oids::*;
+#[derive(Clone, Debug, PartialEq, Eq, Sequence)]
+pub struct RsaSsaPssParams<'a> {
+    #[asn1(context_specific = "0", tag_mode = "EXPLICIT")]
+    hash_algorithm: AlgorithmIdentifier<'a>,
+
+    #[asn1(context_specific = "1", tag_mode = "EXPLICIT")]
+    mask_algorithm: AlgorithmIdentifier<'a>,
+
+    #[asn1(context_specific = "2", tag_mode = "EXPLICIT")]
+    salt_length: u32,
+
+    #[asn1(context_specific = "3", tag_mode = "EXPLICIT")]
+    trailer_field: u32,
+}
 
 pub trait SubjectPublicKeyInfoExt {
     /// Verifies a signature
@@ -17,22 +35,44 @@ pub trait SubjectPublicKeyInfoExt {
 
 impl<'a> SubjectPublicKeyInfoExt for SubjectPublicKeyInfo<'a> {
     fn verify(&self, body: &[u8], algo: AlgorithmIdentifier, sign: &[u8]) -> Result<()> {
-        match (self.algorithm.oids()?, algo.oids()?) {
-            ((ECPUBKEY, Some(NISTP256)), (ECDSA_SHA256, None)) => {
-                use ring::signature::{UnparsedPublicKey, ECDSA_P256_SHA256_ASN1 as ALG};
-                let upk = UnparsedPublicKey::new(&ALG, self.subject_public_key);
-                upk.verify(body, sign)?;
-                Ok(())
-            }
+        let alg: &'static dyn VerAlg = match (self.algorithm.oids()?, (algo.oid, algo.parameters)) {
+            ((ECPUBKEY, Some(NISTP256)), (ECDSA_SHA256, None)) => &ECDSA_P256_SHA256_ASN1,
+            ((ECPUBKEY, Some(NISTP384)), (ECDSA_SHA384, None)) => &ECDSA_P384_SHA384_ASN1,
+            ((RSA_ES_PKCS1_V1_5, None), (RSA_SSA_PSS, Some(p))) => {
+                // Decompose the RSA PSS parameters.
+                let RsaSsaPssParams {
+                    hash_algorithm: hash,
+                    mask_algorithm: mask,
+                    salt_length: salt,
+                    trailer_field: tfld,
+                } = p.decode_into()?;
 
-            ((ECPUBKEY, Some(NISTP384)), (ECDSA_SHA384, None)) => {
-                use ring::signature::{UnparsedPublicKey, ECDSA_P384_SHA384_ASN1 as ALG};
-                let upk = UnparsedPublicKey::new(&ALG, self.subject_public_key);
-                upk.verify(body, sign)?;
-                Ok(())
-            }
+                // Validate the sanity of the mask algorithm.
+                let algo = match (mask.oid, mask.parameters) {
+                    (RSA_MGF1, Some(p)) => {
+                        let p = p.decode_into::<AlgorithmIdentifier>()?;
+                        match (p.oids()?, salt, tfld) {
+                            ((SHA256, None), 32, 1) => Ok(SHA256),
+                            ((SHA384, None), 48, 1) => Ok(SHA384),
+                            ((SHA512, None), 64, 1) => Ok(SHA512),
+                            _ => Err(anyhow!("unsupported")),
+                        }
+                    }
+                    _ => Err(anyhow!("unsupported")),
+                }?;
 
-            _ => Err(anyhow!("unsupported")),
-        }
+                // Prepare for validation.
+                match (hash.oids()?, algo) {
+                    ((SHA256, None), SHA256) => &RSA_PSS_2048_8192_SHA256,
+                    ((SHA384, None), SHA384) => &RSA_PSS_2048_8192_SHA384,
+                    ((SHA512, None), SHA512) => &RSA_PSS_2048_8192_SHA512,
+                    _ => return Err(anyhow!("unsupported")),
+                }
+            }
+            _ => return Err(anyhow!("unsupported")),
+        };
+
+        let upk = UnparsedPublicKey::new(alg, self.subject_public_key);
+        Ok(upk.verify(body, sign)?)
     }
 }
