827c5a6ec6ffdf2320b7947d2c89d60447a0a039
Richard Zak <richard@profian.com>
2022-09-23 16:13:09 -0400

    feat: allow one or more CSRs per request
    Signed-off-by: Richard Zak <richard@profian.com>

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,8 @@ use std::time::{Duration, SystemTime};
 
 use anyhow::Context;
 use axum::body::Bytes;
-use axum::extract::Extension;
+use axum::extract::{Extension, TypedHeader};
+use axum::headers::ContentType;
 use axum::response::IntoResponse;
 use axum::routing::{get, post};
 use axum::Router;
@@ -52,6 +53,9 @@ use x509::request::{CertReq, ExtensionReq};
 use x509::time::{Time, Validity};
 use x509::{Certificate, TbsCertificate};
 use zeroize::Zeroizing;
+
+const PKCS10: &str = "application/pkcs10";
+const BUNDLE: &str = "application/vnd.steward.pkcs10-bundle.v1";
 
 /// Attestation server for use with Enarx.
 ///
@@ -377,6 +381,7 @@ fn attest_request(
 /// Returns:
 /// ASN.1 SEQUENCE OF Output.
 async fn attest(
+    TypedHeader(ct): TypedHeader<ContentType>,
     body: Bytes,
     Extension(state): Extension<Arc<State>>,
 ) -> Result<Vec<u8>, impl IntoResponse> {
@@ -392,10 +397,15 @@ async fn attest(
         not_after: Time::try_from(end).or(Err(StatusCode::INTERNAL_SERVER_ERROR))?,
     };
 
+    // Check for correct mime type.
+    let reqs = match ct.to_string().as_ref() {
+        PKCS10 => vec![CertReq::from_der(body.as_ref()).or(Err(StatusCode::BAD_REQUEST))?],
+        BUNDLE => Vec::from_der(body.as_ref()).or(Err(StatusCode::BAD_REQUEST))?,
+        _ => return Err(StatusCode::BAD_REQUEST),
+    };
+
     // Decode and verify the certification requests.
-    Vec::<CertReq<'_>>::from_der(body.as_ref())
-        .or(Err(StatusCode::BAD_REQUEST))?
-        .into_iter()
+    reqs.into_iter()
         .map(|cr| {
             // Create the basic subject alt name.
             let name = Ia5StringRef::new("foo.bar.hub.profian.com")
@@ -411,15 +421,20 @@ async fn attest(
         })
         .collect::<Result<Vec<_>, _>>()
         .and_then(|issued| {
-            let issued = issued
+            let issued: Vec<Certificate<'_>> = issued
                 .iter()
                 .map(|c| Certificate::from_der(c).or(Err(StatusCode::INTERNAL_SERVER_ERROR)))
                 .collect::<Result<_, _>>()?;
-            Output {
-                chain: vec![issuer],
-                issued,
+
+            match ct.to_string().as_ref() {
+                PKCS10 => vec![issuer, issued[0].clone()].to_vec(),
+                BUNDLE => Output {
+                    chain: vec![issuer],
+                    issued,
+                }
+                .to_vec(),
+                _ => return Err(StatusCode::BAD_REQUEST),
             }
-            .to_vec()
             .or(Err(StatusCode::INTERNAL_SERVER_ERROR))
         })
 }
@@ -438,8 +453,10 @@ mod tests {
         use x509::{ext::Extension, name::RdnSequence};
 
         use axum::response::Response;
+        use http::header::CONTENT_TYPE;
         use http::Request;
         use hyper::Body;
+        use rstest::rstest;
         use tower::ServiceExt; // for `app.oneshot()`
 
         fn certificates_state() -> State {
@@ -450,7 +467,7 @@ mod tests {
             State::generate(None, "localhost").unwrap()
         }
 
-        fn cr(curve: ObjectIdentifier, exts: Vec<Extension<'_>>) -> Vec<u8> {
+        fn cr(curve: ObjectIdentifier, exts: Vec<Extension<'_>>, multi: bool) -> Vec<u8> {
             let pki = PrivateKeyInfo::generate(curve).unwrap();
             let pki = PrivateKeyInfo::from_der(pki.as_ref()).unwrap();
             let spki = pki.public_key().unwrap();
@@ -471,18 +488,26 @@ mod tests {
             };
 
             // Sign the request.
-            vec![CertReq::from_der(&cri.sign(&pki).unwrap()).unwrap()]
-                .to_vec()
-                .unwrap()
+            let signed = cri.sign(&pki).unwrap();
+            if multi {
+                vec![CertReq::from_der(&signed).unwrap()].to_vec().unwrap()
+            } else {
+                signed
+            }
         }
 
-        async fn attest_response(state: State, response: Response) {
+        async fn attest_response(state: State, response: Response, multi: bool) {
             let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
 
-            let response = Output::from_der(body.as_ref()).unwrap();
+            let path = if multi {
+                let response = Output::from_der(body.as_ref()).unwrap();
+                let mut path = PkiPath::from(response.chain);
+                path.push(response.issued[0].clone());
+                path
+            } else {
+                PkiPath::from_der(&body).unwrap()
+            };
 
-            let mut path = PkiPath::from(response.chain);
-            path.push(response.issued[0].clone());
             let issr = Certificate::from_der(&state.crt).unwrap();
             assert_eq!(2, path.len());
             assert_eq!(issr, path[0]);
@@ -490,8 +515,8 @@ mod tests {
         }
 
         #[test]
-        fn reencode() {
-            let encoded = cr(SECP_256_R_1, vec![]);
+        fn reencode_multi() {
+            let encoded = cr(SECP_256_R_1, vec![], true);
             let crs = Vec::<CertReq<'_>>::from_der(&encoded).unwrap();
             assert_eq!(crs.len(), 1);
 
@@ -501,8 +526,19 @@ mod tests {
             assert_eq!(encoded, reencoded);
         }
 
+        #[test]
+        fn reencode_single() {
+            let encoded = cr(SECP_256_R_1, vec![], false);
+            let decoded = CertReq::from_der(&encoded).unwrap();
+            let reencoded = decoded.to_vec().unwrap();
+            assert_eq!(encoded, reencoded);
+        }
+
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn kvm_certs() {
+        async fn kvm_certs(#[case] header: &str, #[case] multi: bool) {
             let ext = Extension {
                 extn_id: Kvm::OID,
                 critical: false,
@@ -512,16 +548,20 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_256_R_1, vec![ext])))
+                .header(CONTENT_TYPE, header)
+                .body(Body::from(cr(SECP_256_R_1, vec![ext], multi)))
                 .unwrap();
 
             let response = app(certificates_state()).oneshot(request).await.unwrap();
             assert_eq!(response.status(), StatusCode::OK);
-            attest_response(certificates_state(), response).await;
+            attest_response(certificates_state(), response, multi).await;
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn kvm_hostname() {
+        async fn kvm_hostname(#[case] header: &str, #[case] multi: bool) {
             let ext = Extension {
                 extn_id: Kvm::OID,
                 critical: false,
@@ -531,15 +571,18 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_256_R_1, vec![ext])))
+                .header(CONTENT_TYPE, header)
+                .body(Body::from(cr(SECP_256_R_1, vec![ext], multi)))
                 .unwrap();
 
             let state = hostname_state();
             let response = app(state.clone()).oneshot(request).await.unwrap();
             assert_eq!(response.status(), StatusCode::OK);
-            attest_response(state, response).await;
+            attest_response(state, response, multi).await;
         }
 
+        // Though similar to the above test, this is the only test which
+        // actually sends many CSRs, versus an array of just one CSR.
         #[tokio::test]
         async fn kvm_hostname_many_certs() {
             let ext = Extension {
@@ -548,7 +591,7 @@ mod tests {
                 extn_value: &[],
             };
 
-            let one_cr_bytes = cr(SECP_256_R_1, vec![ext]);
+            let one_cr_bytes = cr(SECP_256_R_1, vec![ext], true);
             let crs = Vec::<CertReq<'_>>::from_der(&one_cr_bytes).unwrap();
             assert_eq!(crs.len(), 1);
 
@@ -562,6 +605,7 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
+                .header(CONTENT_TYPE, BUNDLE)
                 .body(Body::from(five_crs.to_vec().unwrap()))
                 .unwrap();
 
@@ -575,8 +619,11 @@ mod tests {
             assert_eq!(output.issued.len(), five_crs.len());
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn sgx_certs() {
+        async fn sgx_certs(#[case] header: &str, #[case] multi: bool) {
             for quote in [
                 include_bytes!("ext/sgx/quote.unknown").as_slice(),
                 include_bytes!("ext/sgx/quote.icelake").as_slice(),
@@ -590,17 +637,21 @@ mod tests {
                 let request = Request::builder()
                     .method("POST")
                     .uri("/")
-                    .body(Body::from(cr(SECP_256_R_1, vec![ext])))
+                    .header(CONTENT_TYPE, header)
+                    .body(Body::from(cr(SECP_256_R_1, vec![ext], multi)))
                     .unwrap();
 
                 let response = app(certificates_state()).oneshot(request).await.unwrap();
                 assert_eq!(response.status(), StatusCode::OK);
-                attest_response(certificates_state(), response).await;
+                attest_response(certificates_state(), response, multi).await;
             }
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn sgx_hostname() {
+        async fn sgx_hostname(#[case] header: &str, #[case] multi: bool) {
             for quote in [
                 include_bytes!("ext/sgx/quote.unknown").as_slice(),
                 include_bytes!("ext/sgx/quote.icelake").as_slice(),
@@ -614,18 +665,22 @@ mod tests {
                 let request = Request::builder()
                     .method("POST")
                     .uri("/")
-                    .body(Body::from(cr(SECP_256_R_1, vec![ext])))
+                    .header(CONTENT_TYPE, header)
+                    .body(Body::from(cr(SECP_256_R_1, vec![ext], multi)))
                     .unwrap();
 
                 let state = hostname_state();
                 let response = app(state.clone()).oneshot(request).await.unwrap();
                 assert_eq!(response.status(), StatusCode::OK);
-                attest_response(state, response).await;
+                attest_response(state, response, multi).await;
             }
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn snp_certs() {
+        async fn snp_certs(#[case] header: &str, #[case] multi: bool) {
             let evidence = ext::snp::Evidence {
                 vcek: Certificate::from_der(include_bytes!("ext/snp/milan.vcek")).unwrap(),
                 report: include_bytes!("ext/snp/milan.rprt"),
@@ -642,16 +697,20 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_384_R_1, vec![ext])))
+                .header(CONTENT_TYPE, header)
+                .body(Body::from(cr(SECP_384_R_1, vec![ext], multi)))
                 .unwrap();
 
             let response = app(certificates_state()).oneshot(request).await.unwrap();
             assert_eq!(response.status(), StatusCode::OK);
-            attest_response(certificates_state(), response).await;
+            attest_response(certificates_state(), response, multi).await;
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn snp_hostname() {
+        async fn snp_hostname(#[case] header: &str, #[case] multi: bool) {
             let evidence = ext::snp::Evidence {
                 vcek: Certificate::from_der(include_bytes!("ext/snp/milan.vcek")).unwrap(),
                 report: include_bytes!("ext/snp/milan.rprt"),
@@ -668,21 +727,26 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_384_R_1, vec![ext])))
+                .header(CONTENT_TYPE, header)
+                .body(Body::from(cr(SECP_384_R_1, vec![ext], multi)))
                 .unwrap();
 
             let state = hostname_state();
             let response = app(state.clone()).oneshot(request).await.unwrap();
             assert_eq!(response.status(), StatusCode::OK);
-            attest_response(state, response).await;
+            attest_response(state, response, multi).await;
         }
 
+        #[rstest]
+        #[case(PKCS10, false)]
+        #[case(BUNDLE, true)]
         #[tokio::test]
-        async fn err_no_attestation_certs() {
+        async fn err_no_attestation_certs(#[case] header: &str, #[case] multi: bool) {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_256_R_1, vec![])))
+                .header(CONTENT_TYPE, header)
+                .body(Body::from(cr(SECP_256_R_1, vec![], multi)))
                 .unwrap();
 
             let response = app(certificates_state()).oneshot(request).await.unwrap();
@@ -694,11 +758,27 @@ mod tests {
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
-                .body(Body::from(cr(SECP_256_R_1, vec![])))
+                .header(CONTENT_TYPE, BUNDLE)
+                .body(Body::from(cr(SECP_256_R_1, vec![], true)))
                 .unwrap();
 
             let response = app(hostname_state()).oneshot(request).await.unwrap();
             assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+        }
+
+        #[rstest]
+        #[case(false)]
+        #[case(true)]
+        #[tokio::test]
+        async fn err_no_content_type(#[case] multi: bool) {
+            let request = Request::builder()
+                .method("POST")
+                .uri("/")
+                .body(Body::from(cr(SECP_256_R_1, vec![], multi)))
+                .unwrap();
+
+            let response = app(certificates_state()).oneshot(request).await.unwrap();
+            assert_eq!(response.status(), StatusCode::BAD_REQUEST);
         }
 
         #[tokio::test]
@@ -727,13 +807,14 @@ mod tests {
 
         #[tokio::test]
         async fn err_bad_csr_sig() {
-            let mut cr = cr(SECP_256_R_1, vec![]);
+            let mut cr = cr(SECP_256_R_1, vec![], true);
             let last = cr.last_mut().unwrap();
             *last = last.wrapping_add(1); // Modify the signature...
 
             let request = Request::builder()
                 .method("POST")
                 .uri("/")
+                .header(CONTENT_TYPE, BUNDLE)
                 .body(Body::from(cr))
                 .unwrap();
 
