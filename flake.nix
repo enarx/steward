f45a1c18c822f4237621d2114f08fe94fa5dcb9b
Roman Volosatovs <roman@profian.com>
2022-10-05 20:20:55 +0200

    build: use crane and rust-overlay
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
 {
   description = "Profian Steward";
 
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.fenix.url = github:nix-community/fenix;
+  inputs.crane.inputs.flake-compat.follows = "flake-compat";
+  inputs.crane.inputs.flake-utils.follows = "flake-utils";
+  inputs.crane.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.crane.url = github:ipetkov/crane;
+  inputs.enarx.url = github:enarx/enarx;
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
@@ -13,68 +16,63 @@
 
   outputs = {
     self,
-    fenix,
+    crane,
+    enarx,
     flake-utils,
     nixpkgs,
+    rust-overlay,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "powerpc64le-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ] (
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [];
+          overlays = [(import rust-overlay)];
         };
 
-        devRust = fenix.packages.${system}.fromToolchainFile {file = "${self}/rust-toolchain.toml";};
+        # TODO: Add and use an overlay
+        enarxBin = enarx.packages.${system}.default;
+
+        rust = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
 
         cargo.toml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
+        src =
+          pkgs.nix-gitignore.gitignoreRecursiveSource [
+            "*.nix"
+            "*.yml"
+            "/.github"
+            "flake.lock"
+            "LICENSE"
+            "rust-toolchain.toml"
+          ]
+          self;
 
-        buildPackage = targetPkgs: rustTargets: extraArgs: let
-          rust = with fenix.packages.${system};
-            combine (
-              [
-                minimal.cargo
-                minimal.rustc
-              ]
-              ++ map (target: targets.${target}.latest.rust-std) rustTargets
-            );
-        in
-          (targetPkgs.makeRustPlatform {
-            rustc = rust;
-            cargo = rust;
-          })
-          .buildRustPackage
-          (extraArgs
-            // {
-              inherit (cargo.toml.package) name version;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust;
 
-              src =
-                pkgs.nix-gitignore.gitignoreRecursiveSource [
-                  "*.nix"
-                  "*.yml"
-                  "/.github"
-                  "flake.lock"
-                  "LICENSE"
-                  "rust-toolchain.toml"
-                ]
-                self;
+        commonArgs = {
+          pname = cargo.toml.package.name;
+          inherit (cargo.toml.package) version;
+          inherit src;
+        };
 
-              cargoLock.lockFileContents = builtins.readFile "${self}/Cargo.lock";
-            });
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        buildPackage = extraArgs: craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;} // extraArgs);
 
-        nativeBin = buildPackage pkgs [] {};
-        x86_64LinuxMuslBin =
-          buildPackage (import nixpkgs {
-            inherit system;
-            crossSystem = {
-              config = "x86_64-unknown-linux-musl";
-            };
-            overlays = [];
-          }) [
-            "x86_64-unknown-linux-musl"
-          ] {
-            meta.mainProgram = cargo.toml.package.name;
-          };
+        nativeBin = buildPackage {};
+        # TODO: Add wasm32-wasi support
+        #wasm32WasiBin = buildPackage {
+        #  CARGO_BUILD_TARGET = "wasm32-wasi";
+        #};
+        x86_64LinuxMuslBin = buildPackage {
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
 
         buildImage = bin:
           pkgs.dockerTools.buildImage {
@@ -89,19 +87,23 @@
       in {
         formatter = pkgs.alejandra;
 
-        packages = {
-          "${cargo.toml.package.name}" = nativeBin;
-          "${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
-          "${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
-        };
+        packages."${cargo.toml.package.name}" = nativeBin;
+        # TODO: Add wasm32-wasi support
+        #packages."${cargo.toml.package.name}-wasm32-wasi" = wasm32WasiBin;
+        packages."${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
+        packages."${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
         packages.default = nativeBin;
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.openssl
+          buildInputs =
+            [
+              pkgs.openssl
+              pkgs.wasmtime
 
-            devRust
-          ];
+              rust
+            ]
+            # TODO: Add Enarx, once an overlay is created in enarx
+            ++ (pkgs.lib.optional (system != "powerpc64le-linux") enarxBin);
         };
       }
     );
