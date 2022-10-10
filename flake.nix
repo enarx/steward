0d18ffb97240a5cb45a1b9ccb164fb56dae2b450
Roman Volosatovs <roman@profian.com>
2022-10-10 13:57:19 +0200

    build: enable wasm builds on powerpc
    - Reorganize the flake to better match `enarx`
        - Introduce an overlay
    
    - Add Enarx dependency via overlay
    
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.crane.inputs.flake-utils.follows = "flake-utils";
   inputs.crane.inputs.nixpkgs.follows = "nixpkgs";
   inputs.crane.url = github:ipetkov/crane;
-  inputs.enarx.url = github:enarx/enarx;
+  inputs.enarx.url = github:rvolosatovs/enarx/build/crane; # TODO: Remove fork once https://github.com/enarx/enarx/pull/2269 is merged
   inputs.flake-compat.flake = false;
   inputs.flake-compat.url = github:edolstra/flake-compat;
   inputs.flake-utils.url = github:numtide/flake-utils;
@@ -23,8 +23,90 @@
     rust-overlay,
     ...
   }:
-    with flake-utils.lib.system;
-      flake-utils.lib.eachSystem [
+    with flake-utils.lib.system; let
+      version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+
+      overlay = final: prev: let
+        src =
+          final.nix-gitignore.gitignoreRecursiveSource [
+            "*.nix"
+            "*.yml"
+            "/.github"
+            "flake.lock"
+            "LICENSE"
+            "rust-toolchain.toml"
+          ]
+          ./.;
+
+        rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        craneLib = (crane.mkLib final).overrideToolchain rustToolchain;
+
+        commonArgs = {
+          inherit
+            src
+            version
+            ;
+          pname = "steward";
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        commonArtifactArgs = commonArgs // {inherit cargoArtifacts;};
+
+        checks.clippy = craneLib.cargoClippy (commonArtifactArgs // {cargoClippyExtraArgs = "--all-targets --workspace -- --deny warnings";});
+        checks.fmt = craneLib.cargoFmt commonArtifactArgs;
+        checks.nextest = craneLib.cargoNextest commonArtifactArgs;
+
+        buildPackage = extraArgs:
+          craneLib.buildPackage (commonArtifactArgs
+            // {
+              cargoExtraArgs = "-j $NIX_BUILD_CORES";
+            }
+            // extraArgs);
+
+        nativeBin = buildPackage {};
+        aarch64LinuxMuslBin = buildPackage {
+          CARGO_BUILD_TARGET = "aarch64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+        wasm32WasiBin = buildPackage {
+          nativeBuildInputs = [final.enarx];
+
+          CARGO_BUILD_TARGET = "wasm32-wasi";
+          CARGO_TARGET_WASM_WASI32_RUNNER = "enarx run --wasmcfgfile ${self}/Enarx.toml";
+        };
+        x86_64LinuxMuslBin = buildPackage {
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+
+        buildImage = bin:
+          final.dockerTools.buildImage {
+            name = "steward";
+            tag = version;
+            contents = [
+              bin
+            ];
+            config.Cmd = ["steward"];
+            config.Env = ["PATH=${bin}/bin"];
+          };
+      in {
+        steward = nativeBin;
+        steward-aarch64-unknown-linux-musl = aarch64LinuxMuslBin;
+        steward-aarch64-unknown-linux-musl-oci = buildImage aarch64LinuxMuslBin;
+        steward-wasm32-wasi = wasm32WasiBin;
+        steward-x86_64-unknown-linux-musl = x86_64LinuxMuslBin;
+        steward-x86_64-unknown-linux-musl-oci = buildImage x86_64LinuxMuslBin;
+
+        stewardChecks = checks;
+        stewardRustToolchain = rustToolchain;
+      };
+    in
+      {
+        overlays.default = overlay;
+      }
+      // flake-utils.lib.eachSystem [
         aarch64-darwin
         aarch64-linux
         powerpc64le-linux
@@ -34,95 +116,37 @@
         system: let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [(import rust-overlay)];
+            overlays = [
+              rust-overlay.overlays.default
+              enarx.overlays.default
+              overlay
+            ];
           };
-
-          # TODO: Add and use an overlay
-          enarxBin = enarx.packages.${system}.default;
-
-          rust = pkgs.rust-bin.fromRustupToolchainFile "${self}/rust-toolchain.toml";
-
-          cargo.toml = builtins.fromTOML (builtins.readFile "${self}/Cargo.toml");
-          src =
-            pkgs.nix-gitignore.gitignoreRecursiveSource [
-              "*.nix"
-              "*.yml"
-              "/.github"
-              "flake.lock"
-              "LICENSE"
-              "rust-toolchain.toml"
-            ]
-            self;
-
-          craneLib = (crane.mkLib pkgs).overrideToolchain rust;
-
-          commonArgs = {
-            pname = cargo.toml.package.name;
-            inherit (cargo.toml.package) version;
-            inherit src;
-          };
-
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-          commonArtifactArgs = commonArgs // {inherit cargoArtifacts;};
-
-          cargoClippy = craneLib.cargoClippy (commonArtifactArgs // {cargoClippyExtraArgs = "--all-targets --workspace -- --deny warnings";});
-          cargoFmt = craneLib.cargoFmt commonArtifactArgs;
-          cargoNextest = craneLib.cargoNextest commonArtifactArgs;
-
-          buildPackage = extraArgs: craneLib.buildPackage (commonArtifactArgs // extraArgs);
-          nativeBin = buildPackage {};
-          wasm32WasiBin = buildPackage {
-            nativeBuildInputs = [enarxBin];
-
-            CARGO_BUILD_TARGET = "wasm32-wasi";
-            CARGO_TARGET_WASM_WASI32_RUNNER = "enarx run --wasmcfgfile ${self}/Enarx.toml";
-          };
-          x86_64LinuxMuslBin = buildPackage {
-            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
-          };
-
-          buildImage = bin:
-            pkgs.dockerTools.buildImage {
-              inherit (cargo.toml.package) name;
-              tag = cargo.toml.package.version;
-              contents = [
-                bin
-              ];
-              config.Cmd = [cargo.toml.package.name];
-              config.Env = ["PATH=${bin}/bin"];
-            };
         in {
           formatter = pkgs.alejandra;
 
-          checks.clippy = cargoClippy;
-          checks.fmt = cargoFmt;
-          checks.nextest = cargoNextest;
+          checks = pkgs.stewardChecks;
 
           packages =
             {
-              default = nativeBin;
-
-              "${cargo.toml.package.name}" = nativeBin;
-              "${cargo.toml.package.name}-x86_64-unknown-linux-musl" = x86_64LinuxMuslBin;
-              "${cargo.toml.package.name}-x86_64-unknown-linux-musl-oci" = buildImage x86_64LinuxMuslBin;
+              default = pkgs.steward;
             }
-            # TODO: Remove once an overlay is created in enarx
-            // (pkgs.lib.optionalAttrs (system != powerpc64le-linux) {
-              "${cargo.toml.package.name}-wasm32-wasi" = wasm32WasiBin;
-            });
+            // pkgs.lib.genAttrs [
+              "steward"
+              "steward-aarch64-unknown-linux-musl"
+              "steward-aarch64-unknown-linux-musl-oci"
+              "steward-wasm32-wasi"
+              "steward-x86_64-unknown-linux-musl"
+              "steward-x86_64-unknown-linux-musl-oci"
+            ] (name: pkgs.${name});
 
           devShells.default = pkgs.mkShell {
-            buildInputs =
-              [
-                pkgs.openssl
-                pkgs.wasmtime
-
-                rust
-              ]
-              # TODO: Add Enarx, once an overlay is created in enarx
-              ++ (pkgs.lib.optional (system != powerpc64le-linux) enarxBin);
+            buildInputs = [
+              pkgs.enarx
+              pkgs.openssl
+              pkgs.stewardRustToolchain
+              pkgs.wasmtime
+            ];
           };
         }
       );
